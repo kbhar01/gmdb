7e7bcc6fc847180cab675b19d44300cf306ac683
Bhar <261184@cognizant.com>
2021-03-20 10:05:39 -0500

    Initial checkin

---
+++ src/test/java/com/assesment2/gmdb/GmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment2.gmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
